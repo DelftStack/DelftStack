8d9b169ccd1e96e6259eaf1a7108e6506e865eb3
Jinku Hu <jinkuhu@dishan.nl>
2018-02-27 13:35:35 +0100

    Add Tkinter Label_Get Fonts List.py

---
+++ Tutorial/Tkinter/Tkinter Label_Get Fonts List.py
@@ -0,0 +1,21 @@
+#!/usr/bin
+# -*- coding: utf-8 -*-
+"""
+DelftStack Python Tkinter Tutorial
+
+Author: Jinku Hu
+URL: https://www.delftstack.com/tutorial/tkinter-tutorial/tkinter-label/
+Website: https://www.delftstack.com
+"""
+
+from sys import version_info
+if version_info.major == 2:
+    import Tkinter as tk
+elif version_info.major == 3:
+    import tkinter as tk
+    
+import tkFont
+from pprint import pprint
+
+app = tk.Tk()
+pprint(tkFont.families())
