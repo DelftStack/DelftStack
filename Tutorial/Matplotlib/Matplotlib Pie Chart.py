13d8ca97933e884bc90d4eccc625ebe3a25f42fa
Jinku Hu <jinkuhu@dishan.nl>
2018-08-23 22:18:29 +0200

    Add PyQt5 Grid Layout.py

---
+++ Tutorial/Matplotlib/Matplotlib Pie Chart.py
@@ -0,0 +1,21 @@
+
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+x = np.array([15, 25, 30, 40])
+label = ["France", "Germany", "Uk", "US"]
+
+plt.figure(figsize=(4, 3))
+plt.tight_layout()
+
+plt.pie(x, labels=label)
+plt.title("Pseduo Distribution")
+
+
+import os
+plt.savefig(os.path.join(r"C:\Git\DelftStack\static\img\Matplotlib", 
+                         os.path.basename(__file__).replace('py', 'png')))
+
+plt.show()
---
+++ Tutorial/PyQt5/Grid Layout.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QGridLayout,QPushButton, QApplication)
+
+class basicWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        grid_layout = QGridLayout()
+        self.setLayout(grid_layout)
+
+        for x in range(3):
+            for y in range(3):
+                button = QPushButton(str(str(3*x+y)))
+                grid_layout.addWidget(button, x, y)
+        
+        self.setWindowTitle('Basic Grid Layout')
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    windowExample = basicWindow()
+    windowExample.show()
+    sys.exit(app.exec_())
+    
